284e9b5a6abe85a8ab2e5a11440b2d13feccdfeb
Markus Mayer <widemeadows@gmail.com>
2021-05-16 16:37:03 +0200

    Add taxonomic unit conversion

--- convert_itis_to_jsongraph.py
+++ convert_itis_to_jsongraph.py
@@ -7,6 +7,8 @@ import jsonstreams
 import datetime
 import hashlib
 import json
+from dateutil.parser import parse as parse_time
+from tqdm import tqdm
 
 
 def convert_itis(itis: sqlite3.Connection, itis_md5: str, graph: jsonstreams.Object):
@@ -17,9 +19,11 @@ def convert_itis(itis: sqlite3.Connection, itis_md5: str, graph: jsonstreams.Obj
     with graph.subobject('nodes') as nodes:
         write_kingdom_nodes(itis, nodes)
         write_rank_nodes(itis, nodes)
+        write_taxonomic_unit_nodes(itis, nodes)
 
     with graph.subarray('edges') as edges:
         write_rank_edges(itis, edges)
+        write_taxonomic_unit_edges(itis, edges)
 
 
 def __kingdom_label(kingdom_id: int) -> str:
@@ -90,6 +94,87 @@ def write_rank_edges(itis: sqlite3.Connection, edges: jsonstreams.Array):
                 edge.write('relation', 'required_parent_of')
 
 
+def __taxonomic_unit_label(tsn: int) -> str:
+    return f'tu-{tsn}'
+
+
+def write_taxonomic_unit_nodes(itis: sqlite3.Connection, nodes: jsonstreams.Object):
+    count = itis.execute('SELECT COUNT(*) FROM taxonomic_units WHERE kingdom_id = 3').fetchone()[0]
+
+    units = itis.execute('''
+                        SELECT tsn,
+                               complete_name, name_usage, 
+                               unit_ind1, unit_name1,
+                               unit_ind2, unit_name2,
+                               unit_ind3, unit_name3,
+                               unit_ind4, unit_name4,
+                               initial_time_stamp, update_date
+                        FROM taxonomic_units 
+                        WHERE kingdom_id = 3''')
+
+    for unit in tqdm(units, desc='Units', total=count):
+        tsn = unit[0]
+        complete_name = unit[1]
+        name_usage = unit[2]
+        unit_ind1 = unit[3]
+        unit_name1 = unit[4]
+        unit_ind2 = unit[5]
+        unit_name2 = unit[6]
+        unit_ind3 = unit[7]
+        unit_name3 = unit[8]
+        unit_ind4 = unit[9]
+        unit_name4 = unit[10]
+        initial_time_stamp = unit[11]
+        update_date = unit[12]
+
+        initial_time_stamp = parse_time(initial_time_stamp)
+        is_accepted = True if name_usage == 'accepted' else False
+
+        with nodes.subobject(__taxonomic_unit_label(tsn)) as unit:
+            unit.write('label', complete_name)
+            with unit.subobject('metadata') as meta:
+                if unit_ind1:
+                    meta.write('ind1', unit_ind1)
+                if unit_name1:
+                    meta.write('name1', unit_name1)
+                if unit_ind2:
+                    meta.write('ind2', unit_ind2)
+                if unit_name2:
+                    meta.write('name2', unit_name2)
+                if unit_ind3:
+                    meta.write('ind3', unit_ind3)
+                if unit_name3:
+                    meta.write('name3', unit_name3)
+                if unit_ind4:
+                    meta.write('ind4', unit_ind4)
+                if unit_name4:
+                    meta.write('name4', unit_name4)
+
+                meta.write('tsn', tsn)
+                meta.write('initial_time_stamp', initial_time_stamp.isoformat())
+                meta.write('update_date', update_date)
+                meta.write('accepted', is_accepted)
+
+
+def write_taxonomic_unit_edges(itis: sqlite3.Connection, edges: jsonstreams.Array):
+    count = itis.execute('SELECT COUNT(*) FROM taxonomic_units WHERE kingdom_id = 3').fetchone()[0]
+    units = itis.execute('SELECT rank_id, tsn, parent_tsn FROM taxonomic_units WHERE kingdom_id = 3')
+    for unit in tqdm(units, desc='Unit Relationships', total=count):
+        rank_id = unit[0]
+        tsn = unit[1]
+        parent_tsn = unit[2]
+
+        with edges.subobject() as edge:
+            edge.write('source', __taxonomic_unit_label(parent_tsn))
+            edge.write('target', __taxonomic_unit_label(tsn))
+            edge.write('relation', 'parent_of')
+
+        with edges.subobject() as edge:
+            edge.write('source', __taxonomic_unit_label(tsn))
+            edge.write('target', __rank_label(kingdom_id=3, rank_id=rank_id))
+            edge.write('relation', 'has_rank')
+
+
 def write_graph_attributes(graph, itis_md5):
     graph.write('id', 'itis-042721')
     graph.write('type', 'ITIS')
@@ -119,7 +204,7 @@ def convert_to_dot(input_file: str, output_file: str):
         j = json.load(f)
 
     graph = j['graph']
-    with open(output_file, 'w', encoding='ascii') as f:
+    with open(output_file, 'w', encoding='utf-8') as f:
         f.write(f'digraph "{graph["id"]}" {{\n')
         nodes = graph['nodes']
         for key in nodes:
